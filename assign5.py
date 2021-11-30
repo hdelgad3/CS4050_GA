4e913612d89d8dd0e191ae3a45e09ad15d839d21
hdelgad3 <hdelgado9@outlook.com>
2021-11-29 23:04:49 -0700

    trash crossover

--- assign5.py
+++ assign5.py
@@ -6,6 +6,7 @@ Name: Hector Delgado
 
 import time
 import random
+from queue import PriorityQueue
 
 
 def adjMatFromFile(filename):
@@ -28,9 +29,30 @@ def adjMatFromFile(filename):
     f.close()
     return adjmat
 
+def crossover(parent1, parent2):
+    print("Parent1: ",parent1)
+    print("Parent2: ",parent2)
+    slice1 = parent1[int(len(parent1) * 0.3): int(len(parent1) * 0.7)]
+    slice2 = parent2[int(len(parent2) * 0.3): int(len(parent2) * 0.7)]
+    print(slice1, slice2)
+    values_from_parent2 = parent2[int(len(parent2) * 0.3):] + parent2[:int(len(parent2) * 0.3)]
+    print("values p2: ", values_from_parent2)
+    child1 = []
+    while len(child1) < len(parent1):
+        i = 0
+        if i == int(len(parent1) * 0.3):
+            child1 += slice1
+        else:
+            if values_from_parent2[i] not in slice1:
+                child1.append(values_from_parent2[i])
+        i += 1
+    print("child1: ",child1)
+
+    return 1,1
+
 
 def TSPwGenAlgo(g, max_num_generations=10, population_size=10,
-        mutation_rate=5, explore_rate=5):
+        mutation_rate=0.01, explore_rate=0.6):
     """ A genetic algorithm to attempt to find an optimal solution to TSP  """
 
     def get_path_sum(arr)-> int:
@@ -45,6 +67,7 @@ def TSPwGenAlgo(g, max_num_generations=10, population_size=10,
             fir += 1
             sec += 1
         return total
+
 
     solution_cycle_distance = None # the distance of the final solution cycle/path
     solution_cycle_path = [] # the sequence of vertices representing final sol path to be returned
@@ -75,22 +98,36 @@ def TSPwGenAlgo(g, max_num_generations=10, population_size=10,
     for x in range(max_num_generations):
         # calculate fitness of each individual in the population
         fitness_sums = []
+        sorted_ind = []
 
         for ind in solutions:
             ind_cost = get_path_sum(ind)
             fitness_sums.append(ind_cost)
-        print(fitness_sums)
+            sorted_ind.append((ind_cost,ind))
+        # print(fitness_sums)
+        sorted_ind.sort(key = lambda x: x[0])
+        # print(sorted_ind)
 
 
         # (and append distance of the 'fittest' to shortest_path_each_generation)
-
+        shortest_path_each_generation.append(sorted_ind[0])
         # select the individuals to be used to spawn the generation, then create
         # individuals of the new generation (using some form of crossover)
+
+
+        couples = population[:int(len(population) * (1-explore_rate))]
+
+        parent1 = couples.pop(0)
+        parent2 = couples.pop(0)
+        child1, child2 = crossover(parent1, parent2)
+        new_generation = []
+
+
+
 
         # allow for mutations (this should not happen too often)
 
         # ...
-
     # calculate and verify final solution, and update solution_cycle_distance,
     # solution_path, etc.
 
